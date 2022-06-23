c49ae933e6f2472034a6314a93f2ad33b708e663
David Oseni <ostamdavid1@gmail.com>
2022-06-23 07:38:55 +0100

    my second commit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let var1 = parseInt(prompt("Enter First number"));
+    while (!var1){
+     var1 = parseInt(prompt("Enter a valid number"));
+    }
+
 let operator = prompt("Select an operator from the options: '+, -, *, /");
+    
 let var2 = parseInt(prompt("Enter Second number"));
+while (!var2){
+    var2 = parseInt(prompt("Enter a valid number"));
+   }
 
 if (operator === "+"){
-    console.log(var1 + var2);
+    sol = parseInt(var1 + var2);
 } else if (operator === "-"){
-    console.log(var1 - var2);
+    sol = parseInt(var1 - var2);
 } else if (operator==="*"){
-    console.log(var1 * var2);
+    sol = parseInt(var1 * var2);
 } else if (operator === "/"){
-    console.log(var1 / var2);
+    sol = parseInt(var1 / var2);
 }
+alert(`${var1} ${operator} ${var2} = ${sol}`)
